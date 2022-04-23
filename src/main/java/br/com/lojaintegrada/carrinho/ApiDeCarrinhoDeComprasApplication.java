c68cd59b76510c7c3c80559b5b9e0bea6548d7b6
Thiago dos Anjos <thiagoadosanjos@outlook.com.br>
2022-04-23 19:27:10 -0300

    Realiza configuração inicial do projeto

---
+++ src/main/java/br/com/lojaintegrada/carrinho/ApiDeCarrinhoDeComprasApplication.java
@@ -0,0 +1,13 @@
+package br.com.lojaintegrada.carrinho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDeCarrinhoDeComprasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiDeCarrinhoDeComprasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/lojaintegrada/carrinho/ApiDeCarrinhoDeComprasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lojaintegrada.carrinho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDeCarrinhoDeComprasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
