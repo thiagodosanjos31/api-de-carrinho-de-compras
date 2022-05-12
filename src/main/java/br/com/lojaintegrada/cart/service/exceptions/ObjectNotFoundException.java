46c2b4c47c47bf25da3581ed76b18aa3aa8fea2f
Thiago dos Anjos <thiagoadosanjos@outlook.com.br>
2022-05-11 23:22:26 -0300

    Captura de exceções no fluxo

---
+++ src/main/java/br/com/lojaintegrada/cart/service/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,10 @@
+package br.com.lojaintegrada.cart.service.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException{
+    public ObjectNotFoundException(String msg){
+        super(msg);
+    }
+    public ObjectNotFoundException(String msg, Throwable cause){
+        super(msg, cause);
+    }
+}
